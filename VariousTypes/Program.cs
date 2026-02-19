a923af648ae80ce237461c6590ce120a129d4924
Miguel Martins <miguelarbmartins@gmail.com>
2026-02-19 15:50:53 +0000

    Adicionei variaveis char

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -10,6 +10,10 @@ namespace VariousTypes
             Console.WriteLine(number1);
             uint number2 = 2U;
             Console.WriteLine(number2);
+            char symbol1 = '\uF480';
+            Console.WriteLine(symbol1);
+            char symbol2 = '\uF940';
+            Console.WriteLine(symbol2);
         }
     }
 }
