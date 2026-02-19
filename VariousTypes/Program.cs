89256ec598073945b24e4cc20049300edcb4bc0d
Miguel Martins <miguelarbmartins@gmail.com>
2026-02-19 15:29:07 +0000

    Commit de variaveis inteiras

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number1 = 1;
+            Console.WriteLine(number1);
+            uint number2 = 2U;
+            Console.WriteLine(number2);
+        }
+    }
+}
