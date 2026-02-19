7d06cf8e5db5c61e6043a44e43b8a0aa570624aa
Miguel Martins <miguelarbmartins@gmail.com>
2026-02-19 13:53:36 +0000

    First Commit

---
+++ Programa1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programa1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
